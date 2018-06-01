a1ed1803882380d373e573f77d7939ddc278b0f6
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2018-06-01 16:47:25 +0200

    Added base path to WatServletServer

--- src/de/aitools/aq/wat/WatServletServer.java
+++ src/de/aitools/aq/wat/WatServletServer.java
@@ -13,6 +13,8 @@ public class WatServletServer {
   public static final String SERVLET_PATH = "/wat";
   
   private static final int DEFAULT_PORT = 2112;
+
+  private static final String DEFAULT_BASE_PATH = "/";
   
   private static final String EMBEDDED_RESOURCES_DIR = "web";
   
@@ -20,12 +22,14 @@ public class WatServletServer {
   
   public static void main(final String[] args) throws Exception {
     int port = DEFAULT_PORT;
+    String basePath = DEFAULT_BASE_PATH;
     if (args.length > 0) { port = Integer.parseInt(args[0]); }
+    if (args.length > 1) { basePath = args[1]; }
     System.out.println("Starting WAT on port " + port);
     final Server server = new Server(port);
 
     final ServletContextHandler context = new ServletContextHandler();
-    context.setContextPath("/");
+    context.setContextPath(basePath);
     context.setBaseResource(WatServletServer.createBaseResource());
     server.setHandler(context);
     
@@ -39,6 +43,12 @@ public class WatServletServer {
     context.addServlet(resourcesServlet, "/");
     
     server.start();
+    System.out.println("Access the server at: http://localhost:" + port + basePath);
+    if (basePath.equals("/")) {
+      System.out.println("Direct annotators to: http://localhost:" + port + SERVLET_PATH);
+    } else {
+      System.out.println("Direct annotators to: http://localhost:" + port + basePath + SERVLET_PATH);
+    }
     server.join();
   }
   
